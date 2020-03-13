04f51634fab13cd4cfc679680180dd43d9201605
ankita <ankimitr@cisco.com>
2020-03-13 16:32:24 +0530

    routes setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 5000;
+
+//Connect Database;
+connectDB();
+
+app.get('/', (req, res) => res.send('API is running.'))
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}.`));
