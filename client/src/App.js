6f2b61a9a4c68739eef37b594426bfde34fbb2be
ankita <ankimitr@cisco.com>
2020-03-24 12:20:49 +0530

    alert redux

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,30 @@ import Navbar from "./components/layouts/Navbar";
 import Landing from "./components/layouts/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Alert from "./components/layouts/Alert";
+
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 import './App.css';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar/>
-      <Route exact path="/" component={ Landing }/>
-      <Switch>
-        <section className="container">
-          <Route exact path="/register" component={ Register }/>
-          <Route exact path="/login" component={ Login }/>
-        </section>
-      </Switch>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar/>
+        <Route exact path="/" component={ Landing }/>
+          <section className="container">
+            <Alert />
+            <Switch>
+              <Route exact path="/register" component={ Register }/>
+              <Route exact path="/login" component={ Login }/>
+            </Switch>
+          </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, {Fragment, useState} from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const {email, password} = formData;
+
+    const onChange = event => setFormData({...formData, [event.target.name]: event.target.value});
+
+    const onSubmit = async event => {
+        event.preventDefault();
+        console.log('SUCCESS!');
+    };
+
     return (
-        <div>
-            Login
-        </div>
+        <Fragment>
+            <h1 className="large text-primary">Sign Up</h1>
+            <p className="lead"><i className="fas fa-user"></i> Sign In to your account!</p>
+            <form className="form" onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                <input
+                  type="email"
+                  placeholder="Email Address"
+                  name="email"
+                  value={email}
+                  onChange={ e => onChange(e)}
+                />
+                <small className="form-text"
+                    >This site uses Gravatar so if you want a profile image, use a
+                    Gravatar email</small
+                >
+                </div>
+                <div className="form-group">
+                <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    minLength="6"
+                    value={password}
+                    onChange={ e => onChange(e)}
+                />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Login" />
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to="/register">Sign Up</Link>
+            </p>
+        </Fragment>
     )
 };
 
 export default Login;
+
