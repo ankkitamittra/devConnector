b577cdb6dfe81cca393906afeda18a67638d73f3
ankita <ankimitr@cisco.com>
2020-04-03 11:07:51 +0530

    producation

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
-const PORT = process.env.PORT || 5000;
+const path = require('path');
+
 
 //Connect Database;
 connectDB();
@@ -9,7 +10,7 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API is running.'))
+//app.get('/', (req, res) => res.send('API is running.'))
 
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -17,4 +18,13 @@ app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 
+//server static assets in production
+if(process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}.`));
